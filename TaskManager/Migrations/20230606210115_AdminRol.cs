113179ce7b3d73253aaeb432a737a0e93659bd61
AlbertoJTD <albertojtd@outlook.com>
2023-06-06 15:11:02 -0600

    Add admin roole

---
+++ TaskManager/Migrations/20230606210115_AdminRol.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                IF NOT EXISTS(SELECT Id FROM AspNetRoles WHERE Id = 'f03aa6c1-f77c-4a04-8e4c-b3d6b8b43dbd')
+                BEGIN
+	                INSERT AspNetRoles (Id, [Name], [NormalizedName])
+	                VALUES ('f03aa6c1-f77c-4a04-8e4c-b3d6b8b43dbd', 'admin', 'ADMIN')
+                END");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE AspNetRoles WHERE Id = 'f03aa6c1-f77c-4a04-8e4c-b3d6b8b43dbd'");
+        }
+    }
+}
