1aa83c1a3aefb958a14c6873c7c0f9fdfedb25bb
AlbertoJTD <albertojtd@outlook.com>
2023-06-09 21:05:17 -0600

    Add API controller type

---
+++ TaskManager/Controllers/TareasController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Servicios;
+
+namespace TaskManager.Controllers
+{
+	[Route("api/tareas")]
+	public class TareasController: ControllerBase
+	{
+		private readonly ApplicationDbContext context;
+		private readonly IServicioUsuarios servicioUsuarios;
+
+		public TareasController(ApplicationDbContext context, IServicioUsuarios servicioUsuarios)
+        {
+			this.context = context;
+			this.servicioUsuarios = servicioUsuarios;
+		}
+
+
+    }
+}
